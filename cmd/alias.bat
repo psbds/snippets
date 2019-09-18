16411fe59e7e7d45cad29c2bf077c2ab11cc5566
Paulo Sérgio B <padasil@microsoft.com>
2019-09-17 22:14:37 -0300

    adding more snippets

--- cmd/alias.bat
+++ cmd/alias.bat
@@ -2,6 +2,10 @@ doskey kgp=kubectl get pods $*
 doskey kgs=kubectl get svc $*
 doskey kgd=kubectl get deploy $*
 doskey kgi=kubectl get ingress $*
-doskey kgi=kubectl get ingress $*
+doskey kss=kubectl get statefulsets $*
+doskey kpv=kubectl get pv $*
+doskey kpvc=kubectl get pvc $*
 doskey kns=kubectl config set-context --current --namespace=$*
 doskey kctx=kubectl config use-context $*
+
+cls
